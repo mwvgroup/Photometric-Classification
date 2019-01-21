31c0684a2a95eafdf5fa7a4f406c1f2e39ad6bf7
djperrefort <djperrefort@pitt.edu>
2019-01-21 10:30:01 -0500

    Makes 91bg_model a module

---
+++ 91bg_model/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python2.7
+# -*- coding: UTF-8 -*-
+
+"""This module defines a custom 91bg source for use with SNCosmo. The base
+model is based on Nugent's 91bg model but is extended to the UV with synthetic
+spectra from Hachinger et al. 2008.
+"""
+
+from .sncosmo_ported_model import SN91bgSource
