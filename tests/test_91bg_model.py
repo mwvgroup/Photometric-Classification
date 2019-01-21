8841e2db5e340e34afa6e879feb785399a833642
djperrefort <djperrefort@pitt.edu>
2019-01-21 10:59:43 -0500

    Adds travis tests

---
+++ tests/test_91bg_model.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+"""Test that our custom model for 91bg like supernovae was ported correctly
+for use with SNCosmo.
+"""
+
+from unittest import TestCase
+
+import sncosmo
+
+from sn91bg_model import SN91bgSource
+
+
+class SN91bgModel(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.model = sncosmo.Model(source=SN91bgSource())
+
+    def test_model_runs(self):
+        data = sncosmo.load_example_data()
+        result, fitted_model = sncosmo.fit_lc(
+            data, self.model, ['stretch', 'color', 'amplitude'])
