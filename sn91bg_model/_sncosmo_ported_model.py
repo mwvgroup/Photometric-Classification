a8086fc24e17a5880bbb8dd82fb0c05b322cef8a
djperrefort <djperrefort@pitt.edu>
2019-01-25 12:14:22 -0500

    Auto code inspection results

--- sn91bg_model/_sncosmo_ported_model.py
+++ sn91bg_model/_sncosmo_ported_model.py
@@ -9,58 +9,65 @@ import os
 
 import numpy as np
 import sncosmo
-from scipy.interpolate import RectBivariateSpline,interpn
-import matplotlib.pyplot as plt
+from scipy.interpolate import RectBivariateSpline
 
 FILE_DIR = os.path.abspath(os.path.dirname(__file__))
 COMPILED_MODEL_PATH = os.path.join(FILE_DIR, 'complete_template.npy')
 
-def bi_search(a,x):
+
+def bi_search(a, x):
     """
     Binary search
 
     Args:
-        a   (list): the list in which the number x will be searched
-        x    (num): the number to be searched 
+        a (list): The list in which the number x will be searched
+        x  (num): The number to be searched
 
     Returns:
-        the position of nearest neighbors of the number x
+        The position of nearest neighbors of the number x
     """
+
     if x in a:
         return a.index(x)
-    l,r=0,len(a)
-    while(abs(r-l)>1):
-        m=(l+r)//2
-        if a[m]>x:
-            r=m
-        if a[m]<x:
-            l=m
-    return l,r
 
-def bilinear_interp(x1,x2,y1,y2,f,x,y):
+    l, r = 0, len(a)
+    while abs(r - l) > 1:
+        m = (l + r) // 2
+        if a[m] > x:
+            r = m
+
+        if a[m] < x:
+            l = m
+
+    return l, r
+
+
+def bilinear_interp(x1, x2, y1, y2, f, x, y):
     """
     Bilinear interpolate point (x,y) in the grid (x1,y1), (x1,y2), (x2,y1), (x2,y2)
 
     Args:
-        x1,x2,y1,y2  (num): grid point coordinates
-        x,y          (num): coordinate of the interpolating point
-        f           (list): a list containing four values at (x1,y1), (x1,y2), (x2,y1), (x2,y2)
+        x1,x2,y1,y2  (num): Grid point coordinates
+        x,y          (num): Coordinate of the interpolating point
+        f           (list): A list containing four values at
+                             (x1,y1), (x1,y2), (x2,y1), (x2,y2)
 
     Returns:
         the interpolating value
     """
-    f11=f[0]
-    f12=f[1]
-    f21=f[2]
-    f22=f[3]
-    fy1=(x2-x)*f11/0.1+(x-x1)*f21/0.1
-    fy2=(x2-x)*f12/0.1+(x-x1)*f22/0.1
-    f_interp=(y2-y)*fy1/0.25+(y-y1)*fy2/0.25
+    f11 = f[0]
+    f12 = f[1]
+    f21 = f[2]
+    f22 = f[3]
+    fy1 = (x2 - x) * f11 / 0.1 + (x - x1) * f21 / 0.1
+    fy2 = (x2 - x) * f12 / 0.1 + (x - x1) * f22 / 0.1
+    f_interp = (y2 - y) * fy1 / 0.25 + (y - y1) * fy2 / 0.25
     return f_interp
+
 
 class SN91bgSource(sncosmo.Source):
     _param_names = ['amplitude', 'stretch', 'color']
-    param_names_latex = ['A', 'x1', 'c']   # used in plotting display
+    param_names_latex = ['A', 'x1', 'c']  # used in plotting display
 
     def __init__(self):
         super(SN91bgSource, self).__init__()
@@ -73,16 +80,20 @@ class SN91bgSource(sncosmo.Source):
         self._parameters = np.array([1., 1., 0.55])
 
         # 4-dimension grid points in the model
-        self._stretch = np.array([0.65,0.75,0.85,0.95,1.05,1.15,1.25])
+        self._stretch = np.array([0.65, 0.75, 0.85, 0.95, 1.05, 1.15, 1.25])
         self._color = np.array([0.0, 0.25, 0.5, 0.75, 1])
         self._phase = np.arange(-13, 101, 1)
         self._wave = np.arange(1000, 12001, 10)
 
-        # Creat bi-cubic spline in dimension phase and wavelength for all 35 templates
-        self._model_flux=np.full((len(self._stretch),len(self._color)),None).tolist()
+        # Creat bi-cubic spline for phase and wavelength using all 35 templates
+        self._model_flux = np.full((len(self._stretch), len(self._color)), None)
         for i in range(len(self._stretch)):
             for j in range(len(self._color)):
-                self._model_flux[i][j]=RectBivariateSpline(self._phase, self._wave, self._flux_values[i][j], kx=3, ky=3)
+                self._model_flux[i][j] = RectBivariateSpline(
+                    self._phase,
+                    self._wave,
+                    self._flux_values[i][j], kx=3,
+                    ky=3)
 
     @staticmethod
     def _get_91bg_model():
@@ -111,13 +122,16 @@ class SN91bgSource(sncosmo.Source):
             A 2d array of flux with shape (<len(phase)>, <len(wave)>)
         """
 
-        A, x1, c=self._parameters
+        A, x1, c = self._parameters
 
         # Find the 4 nearest grid points of (x1,c)
-        st1,st2=bi_search(self._stretch,x1)
-        c1,c2=bi_search(self._color,c)
-        f=[self._model_flux[st1][c1](phase,wave),self._model_flux[st1][c2](phase,wave),\
-          self._model_flux[st2][c1](phase,wave),self._model_flux[st2][c2](phase,wave)]
+        st1, st2 = bi_search(self._stretch, x1)
+        c1, c2 = bi_search(self._color, c)
+        f = [self._model_flux[st1][c1](phase, wave),
+             self._model_flux[st1][c2](phase, wave),
+             self._model_flux[st2][c1](phase, wave),
+             self._model_flux[st2][c2](phase, wave)]
 
         # Bilinear interpolation for stretch and color
-        return A*bilinear_interp(self._stretch[st1],self._stretch[st2],self._color[c1],self._color[c2],f,x1,c)
+        return A * bilinear_interp(self._stretch[st1], self._stretch[st2],
+                                   self._color[c1], self._color[c2], f, x1, c)
