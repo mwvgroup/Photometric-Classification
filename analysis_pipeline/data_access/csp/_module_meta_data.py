bea7641b7fc5c5dddc81bc0984b9c19f3bd6dccc
djperrefort <djperrefort@pitt.edu>
2019-03-14 11:50:13 -0400

    Fixes units of flux and flux error in CSP input tables

--- analysis_pipeline/data_access/csp/_module_meta_data.py
+++ analysis_pipeline/data_access/csp/_module_meta_data.py
@@ -43,9 +43,9 @@ _local_filt_names = [
     'V_LC3009_tel_ccd_atm_ext_1.2.dat',  # V1
     'V_tel_ccd_atm_ext_1.2.dat',  # V
     'Y_SWO_TAM_scan_atm.dat',  # Y
-    'H_SWO_TAM_scan_atm.dat',  # H
     'J_old_retrocam_swope_atm.dat',  # J
     'J_SWO_TAM_atm.dat',  # Jrc2
+    'H_SWO_TAM_scan_atm.dat',  # H
     'Y_texas_DUP_atm.dat',  # Ydw
     'J_texas_DUP_atm.dat',  # Jdw
     'H_texas_DUP_atm.dat'  # Hdw
@@ -57,8 +57,11 @@ _band_names = ['u', 'g', 'r', 'i', 'B', 'V0', 'V1', 'V',
 
 band_names = [f'91bg_proj_csp_{f}' for f in _band_names]
 lambda_effective = [3639.3, 4765.1, 6223.3, 7609.2, 4350.6, 5369.6, 5401.4,
-                    5375.2, 10350.8, 16297.7, 12386.5, 12356.3, 10439.8,
+                    5375.2, 10350.8, 12386.5, 12356.3, 16297.7, 10439.8,
                     12383.2, 16282.8]
+
+zero_point = [12.986, 15.111, 14.902, 14.545, 14.328, 14.437, 14.393, 14.439,
+              13.921, 13.836, 13.836, 13.510, 13.770, 13.866, 13.502]
 
 download_data(
     base_url=_csp_url,
