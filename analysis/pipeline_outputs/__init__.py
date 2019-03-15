2e1b1154d735f0fe80b67d74e67b2b48aa8d03b8
djperrefort <djperrefort@pitt.edu>
2019-03-15 13:46:58 -0400

    Changes from Tables to DataFrames

---
+++ analysis/pipeline_outputs/__init__.py
@@ -0,0 +1,24 @@
+import pandas as _pd
+
+
+def get_fit_results(survey, model, params):
+    """Get lightcurve fits for a given survey and model
+
+    Args:
+        survey (str): The name of the survey
+        model  (str): The name of the fitted model
+        params (int): The number of Salt2 params that were fit (4 or 5)
+
+    Returns:
+        A DataFrame of fits in all bands
+        A DataFrame of fits in blue bands
+        A DataFrame of fits in red bands
+    """
+
+    index_col = 0
+    path_pattern = f'./pipeline_outputs/{survey}_{model}_{params}param_{{}}.csv'
+
+    all_data = _pd.read_csv(path_pattern.format('all'), index_col=index_col)
+    blue_data = _pd.read_csv(path_pattern.format('blue'), index_col=index_col)
+    red_data = _pd.read_csv(path_pattern.format('red'), index_col=index_col)
+    return all_data.dropna(), blue_data.dropna(), red_data.dropna()
