f436c10d6e8329996037f84d88d59f4c87364e8f
yiz143 <yiz143@pitt.edu>
2019-06-29 23:26:52 -0400

    Adds import calc_chisq

--- analysis_pipeline/_lc_fitting/__init__.py
+++ analysis_pipeline/_lc_fitting/__init__.py
@@ -4,7 +4,7 @@
 """This module handles the nested sampling and fitting of light curve data."""
 
 from ._fit_funcs import create_empty_summary_table
-from ._fit_funcs import fit_lc
+from ._fit_funcs import fit_lc, calc_chisq
 from ._fit_funcs import get_sampled_model
 from ._fit_funcs import nest_lc
 from ._iter_fitting import iter_all_fits, split_data
