6f22fb9ca26e906430b2e86b4c58af143498dc0b
djperrefort <djperrefort@pitt.edu>
2019-07-03 08:50:22 -0400

    Limits phase of 91bg model

--- analysis_pipeline/_sn91bg_model/_model.py
+++ analysis_pipeline/_sn91bg_model/_model.py
@@ -58,6 +58,7 @@ def linear_interp(x0, x1, f, x):
     Returns:
         Interpolation value
     """
+
     y0 = f[0]
     y1 = f[1]
     y = y0 + ((x - x0) * y1 - (x - x0) * y0) / (x1 - x0)
@@ -72,21 +73,25 @@ class SN91bgSource(sncosmo.Source):
         super(SN91bgSource, self).__init__()
 
         self.name = 'sn91bg'
-        self.version = 'color_interpolation'
+        self.version = 'salt2_phase'
 
-        # Model spectra and initial guess for stretch, color, and amplitude
-        self._flux_values = self._get_91bg_model()[0]
+        # Determine phase range of Salt2.4 model
+        salt2_phase = sncosmo.Model('salt2').source._phase
+        self.phase_lim = min(salt2_phase), max(salt2_phase)
+
+        # Get phase limited 91bg model
+        grid_cords, self._flux_values = self._get_91bg_model(*self.phase_lim[0])
+        self._stretch = grid_cords[0]
+        self._color = grid_cords[1]
+        self._phase = grid_cords[2]
+        self._wave = grid_cords[3]
+
+        # Define initial parameter values
         self._parameters = np.array([1., 1., 0.55])
-
-        # 4-dimension grid points in the model
-        self._stretch = np.array([0.65, 0.75, 0.85, 0.95, 1.05, 1.15, 1.25])
-        self._color = np.array([0.0, 0.25, 0.5, 0.75, 1])
-        self._phase = np.arange(-18, 101, 1)
-        self._wave = np.arange(1000, 12001, 10)
 
         # Creat bi-cubic spline for phase and wavelength using 5 templates
         # (stretch: 0.65, color: [0,0.25,0.5,0.75,1.])
-        self._model_flux = np.full(len(self._color), None)
+        self._model_flux = np.empty(len(self._color), RectBivariateSpline)
         for i in range(len(self._color)):
             self._model_flux[i] = RectBivariateSpline(
                 self._phase,
@@ -95,7 +100,7 @@ class SN91bgSource(sncosmo.Source):
                 kx=3, ky=3)
 
     @staticmethod
-    def _get_91bg_model():
+    def _get_91bg_model(phase_min, phase_max):
         """Load template spectra for SN 1991bg
 
         Template spans 7 stretch values, 5 color values, 114 dates,
@@ -105,7 +110,20 @@ class SN91bgSource(sncosmo.Source):
             An array of shape (7, 5, 114, 1101)
         """
 
-        return np.load(COMPILED_MODEL_PATH)
+        modeled_flux = np.load(COMPILED_MODEL_PATH)[0]
+
+        # 4-dimension grid points in the model
+        stretch = np.array([0.65, 0.75, 0.85, 0.95, 1.05, 1.15, 1.25])
+        color = np.array([0.0, 0.25, 0.5, 0.75, 1])
+        phase = np.arange(-18, 101, 1)
+        wave = np.arange(1000, 12001, 10)
+
+        # Limit model phase range
+        phase_indices = ((phase_min <= phase) & (phase <= phase_max))
+        modeled_flux = modeled_flux[:, phase_indices, :]
+        phase = phase[phase_indices]
+
+        return (stretch, color, phase, wave), modeled_flux
 
     def _flux(self, phase, wave):
         """
@@ -115,8 +133,8 @@ class SN91bgSource(sncosmo.Source):
         Stretch, color, and amplitude are given by self._parameters
 
         Args:
-            phase (list): A list of days till maximum for the desired flux
-            wave  (list): A list of wavelengths for the desired flux
+            phase (ndarray): A list of days till maximum for the desired flux
+            wave  (ndarray): A list of wavelengths for the desired flux
 
         Returns:
             A 2d array of flux with shape (<len(phase)>, <len(wave)>)
@@ -126,7 +144,8 @@ class SN91bgSource(sncosmo.Source):
 
         # Linearly interpolate template flux by color
         if c in self._color:
-            f = self._model_flux[self._color.tolist().index(c)](phase / (st / 0.65), wave)
+            f = self._model_flux[self._color.tolist().index(c)](
+                phase / (st / 0.65), wave)
 
         else:
             c1, c2 = bi_search(self._color, c)
@@ -135,4 +154,8 @@ class SN91bgSource(sncosmo.Source):
 
             f = linear_interp(self._color[c1], self._color[c2], y, c)
 
+        phase_indices = (
+                (phase < self.phase_lim[0]) | (self.phase_lim[1] < phase)
+        )
+        f[phase_indices] = np.zeros(wave.shape)
         return A * f
