0a42f2b16c0a967c45d7ef770152f23bc541f304
djperrefort <djperrefort@pitt.edu>
2019-07-09 21:27:44 -0400

    Fixes syntax typo

--- analysis_pipeline/__init__.py
+++ analysis_pipeline/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: UTF-8 -*-
 
 
-git afrom . import lc_fitting
+from . import lc_fitting
 from . import models
 from .lc_fitting._fit_funcs import get_priors
 from .utils import FIT_DIR, PRIOR_DIR, get_fit_results, save_priors
