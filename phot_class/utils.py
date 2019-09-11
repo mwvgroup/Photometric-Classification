Fixes incorrect chi-squared calculation in utils

phot_class/utils.py
@@ -7,6 +7,7 @@ import signal
 from copy import deepcopy
 
 import numpy as np
+import sncosmo
 from astropy.table import Table
 
 
@@ -49,29 +50,20 @@ def calc_model_chisq(data, result, model):
     """
 
     data = deepcopy(data)
-    data.sort('time')  # To keep sncosmo happy
 
-    while True:
-        try:
-            # Model flux and keep only non-zero values
-            model_flux = model.bandflux(data['band'], data['time'])
+    # Drop any data that is not withing the model's range
+    lambda_effective = [sncosmo.get_bandpass(b).wave_eff for b in data['band']]
+    data = data[
+        (data['time'] >= model.mintime()) &
+        (data['time'] <= model.maxtime()) &
+        (lambda_effective >= model.minwave()) &
+        (lambda_effective <= model.maxwave())
+    ]
 
-        except ValueError as err:
-            if 'outside spectral range' not in str(err):
-                raise  # Something went wrong that we aren't expecting
+    if len(data) == 0:
+        raise ValueError('No data within model range')
 
-            # sncosmo doesn't like bands that are outside of the model's range
-            data = data[data['band'] != err.args[0].split()[1][1:-1]]
-            if len(data) == 0:
-                raise RuntimeError('Ran out of data inside model range!')
-
-        else:
-            data = data[model_flux > 0]
-            model_flux = model_flux[model_flux > 0]
-
-            residuals = model_flux - data['flux']
-            chisq = np.sum((residuals / data['fluxerr']) ** 2)
-            return chisq, len(data) - len(result.vparam_names)
+    return sncosmo.chisq(data, model), len(data) - len(result.vparam_names)
 
 
 def split_bands(bands, lambda_eff):
@@ -143,7 +135,8 @@ def split_data(data_table, band_names, lambda_eff, z, cutoff=700):
 
     # Keep only the specified filters that are within 700 Angstroms of the
     # rest frame effective wavelength
-    is_ok_diff = delta_lambda[min_indx, np.arange(delta_lambda.shape[1])] < cutoff
+    is_ok_diff = delta_lambda[
+                     min_indx, np.arange(delta_lambda.shape[1])] < cutoff
 
     # Split into blue and red band passes
     out_list = []

tests/test_utils.py
@@ -57,7 +57,6 @@ class TestTimeout(TestCase):
         self.assertRaises(TypeError, self.run_timeout, 2.1)
 
 
-# Todo: Write a test that checks we get the correct value for chisq
 class TestCalcModelChisq(TestCase):
     """Tests for utils.calc_model_chisq"""
 
@@ -69,7 +68,7 @@ class TestCalcModelChisq(TestCase):
         """The model to use for testing"""
 
         model = sncosmo.Model('salt2')
-        model.set(z=self.data.meta['z'])
+        model.update(self.data.meta)
         return model
 
     @property
@@ -144,23 +143,52 @@ class TestCalcModelChisq(TestCase):
         self.assertRaises(Exception, func, *args)
 
     def test_unsorted_times(self):
-        """Test the function return is independent of the time order"""
+        """Test an error is not raised when time values are unsorted"""
 
         data = self.data
         data.sort('time')
-        initial_chisq = utils.calc_model_chisq(data, self.results, self.model)
-        print(initial_chisq)
-
         data[0], data[-1] = data[-1], data[0]
-        new_chisq = utils.calc_model_chisq(data, self.results, self.model)
-        self.assertEqual(initial_chisq, new_chisq)
+        utils.calc_model_chisq(data, self.results, self.model)
+
+    def test_correct_chisq(self):
+        """Test the correct chisq and dof are returned for simulated data"""
+
+        # Define necessary information to simulate a table of flux data
+        model = self.model
+        t0 = model.parameters[1]
+        zp_system = 'ab'
+        zero_point = 25
+        band_name = 'sdssg'
+        flux_offset = 100  # Difference between "observed" and model flux
+        flux_err_coeff = .1  # flux error / flux
+
+        # Create table of simulated flux
+        phase = np.arange(-10, 30) + t0
+        model_flux = model.bandflux(band_name, phase, zero_point, zp_system)
+        flux = model_flux + flux_offset
+        flux_err = flux * flux_err_coeff
+        band = np.full(len(phase), band_name)
+        zpsys = np.full(len(phase), zp_system)
+        zp = np.full(len(phase), zero_point)
+
+        data = Table(
+            [phase, band, flux, flux_err, zp, zpsys],
+            names=['time', 'band', 'flux', 'fluxerr', 'zp', 'zpsys']
+        )
+
+        result = sncosmo.utils.Result(
+            {'vparam_names': ['t0', 'x0', 'x1', 'c']})
+
+        expected_chisq = np.sum(((flux - model_flux) / flux_err) ** 2)
+        chisq, dof = utils.calc_model_chisq(data, result, model)
+        self.assertEqual(expected_chisq, chisq)
 
 
 class TestSplitBands(TestCase):
     """Tests for utils.split_bands"""
 
     def runTest(self):
-        """Test dummy bands are correctly seperated into blue and red"""
+        """Test dummy bands are correctly separated into blue and red"""
 
         # Define dummy bands and their effective wavelengths
         band_names = ['blue1', 'red1', 'red2']
@@ -199,8 +227,10 @@ class TestSplitData(TestCase):
                 data, band_names, lambda_eff, redshift, cutoff=float('inf'))
 
             err_msg = f'Wrongs bands for z={redshift}'
-            self.assertListEqual(expected_blue, list(blue_table['band']), err_msg)
-            self.assertListEqual(expected_red, list(red_table['band']), err_msg)
+            self.assertListEqual(expected_blue, list(blue_table['band']),
+                                 err_msg)
+            self.assertListEqual(expected_red, list(red_table['band']),
+                                 err_msg)
 
     def test_maintains_metadata(self):
         """Test whether passed and returned tables have same metadata"""
@@ -213,7 +243,8 @@ class TestSplitData(TestCase):
             lambda_eff=[3550, 4680],
             z=0)
 
-        self.assertIn('dummy_key', blue_data.meta, 'Blue table missing metadata')
+        self.assertIn('dummy_key', blue_data.meta,
+                      'Blue table missing metadata')
         self.assertIn('dummy_key', red_data.meta, 'Red table missing metadata')
 
     def test_missing_effective_wavelength(self):