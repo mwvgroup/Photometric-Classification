22f5e16a2cf584fec02acc306a1c418d4958978a
djperrefort <djperrefort@pitt.edu>
2019-09-11 08:41:49 -0400

    Removes stale pipeline outputs until

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 python run_pipeline.py -s csp -r dr3 -f simple_fit -v t0 x0 x1 c -t 90 -o ./phot_class_results
+python run_pipeline.py -s des -r sn3yr -f simple_fit -v t0 x0 x1 c -t 90 -o ./phot_class_results
