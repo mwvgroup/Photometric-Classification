0e1d093ac11652cd0f52fc7bf0adfa585873ef17
djperrefort <djperrefort@pitt.edu>
2019-09-17 22:38:33 -0400

    Adds priors for DES (uninspected)

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -2,6 +2,14 @@
 
 # Run CSP DR3 and fix redshift to the prior
 python run_pipeline.py \
+    -s des \
+    -r sn3yr \
+    -f simple_fit \
+    -v t0 x0 x1 c \
+    -c 'priors_config.yml' \
+    -o './results';
+
+python run_pipeline.py \
     -s csp \
     -r dr3 \
     -f simple_fit \
