0f843702e8742457d5fa4aff3b285abc7c6c7153
djperrefort <djperrefort@pitt.edu>
2019-09-23 19:01:21 -0400

    Updates table tests

--- tests/test_classification.py
+++ tests/test_classification.py
@@ -24,15 +24,16 @@ class TableCreation(TestCase):
     def test_is_empty(self):
         """Test the returned table is empty by default"""
 
-        num_rows = len(classification.create_empty_table())
+        num_rows = len(classification.create_empty_table([]))
         self.assertEqual(0, num_rows, 'Table is not empty')
 
     def test_correct_columns(self):
         """Test the returned table has the correct columns"""
 
-        returned = classification.create_empty_table().colnames
+        parameters = ['z', 't0', 'x0', 'x1', 'c']
+        returned = classification.create_empty_table(parameters).colnames
         expected = [
-            'obj_id', 'band_set', 'source', 'pre_max', 'post_max',
+            'obj_id', 'band', 'source', 'pre_max', 'post_max',
             'z', 't0', 'x0', 'x1', 'c',
             'z_err', 't0_err', 'x0_err', 'x1_err', 'c_err',
             'chisq', 'ndof', 'b_max', 'delta_15', 'message', ]
@@ -42,15 +43,15 @@ class TableCreation(TestCase):
     def test_is_masked(self):
         """Test the returned table is a masked Table by default"""
 
-        is_masked = classification.create_empty_table().masked
+        is_masked = classification.create_empty_table([]).masked
         self.assertTrue(is_masked, 'Table has no mask')
 
     def test_accepts_kwargs(self):
         """Test that the table constructor uses the kwargs"""
 
-        num_columns = len(classification.create_empty_table().colnames)
+        num_columns = len(classification.create_empty_table([]).colnames)
         dummy_row = np.ones(num_columns)
-        table = classification.create_empty_table(rows=[dummy_row])
+        table = classification.create_empty_table([], rows=[dummy_row])
         self.assertEqual(1, len(table), 'Table did not add specified rows')
 
 
