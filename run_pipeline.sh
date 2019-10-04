16a835d6296bfa5abc3f524edb9592f151737815
djperrefort <djperrefort@pitt.edu>
2019-10-04 09:56:10 -0700

    Updates run file

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -5,13 +5,27 @@ python run_pipeline.py \
     -s des \
     -r sn3yr \
     -f simple_fit \
+    -c 'config_files/des_config_noebv.yml' \
+    -o './results/no_ext';
+
+python run_pipeline.py \
+    -s des \
+    -r sn3yr \
+    -f simple_fit \
     -c 'config_files/des_config.yml' \
-    -o './results';
+    -o './results/with_ext';
 
 # Run SDSS Sako 2018
 python run_pipeline.py \
     -s sdss \
     -r sako18 \
     -f simple_fit \
+    -c 'config_files/sdss_config_noebv.yml' \
+    -o './results/no_ext';
+
+python run_pipeline.py \
+    -s sdss \
+    -r sako18 \
+    -f simple_fit \
     -c 'config_files/sdss_config.yml' \
-    -o './results';
+    -o './results/with_ext';
