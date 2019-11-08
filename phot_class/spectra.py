916a7c9952b19de6d8e8043b9f77d4919aec9d64
djperrefort <djperrefort@pitt.edu>
2019-11-08 10:50:29 -0500

    Minor typo fixes. Port matches IDL

--- phot_class/spectra.py
+++ phot_class/spectra.py
@@ -6,6 +6,8 @@ velocity, and  equivalent width.
 """
 
 import numpy as np
+from astropy import units
+from astropy.constants import c as speed_of_light
 from scipy.optimize import curve_fit
 
 # Todo: Compare outputs to original IDL code
@@ -76,7 +78,7 @@ def feature_pew(wave, flux):
     return norm_flux, pew
 
 
-def feature_velocity(wave, flux, eflux, rest_frame):
+def feature_velocity(wave, flux, eflux, rest_frame, unit=None):
     """Calculate the velocity of a feature
 
     Args:
@@ -84,11 +86,13 @@ def feature_velocity(wave, flux, eflux, rest_frame):
         flux     (ndarray): An array of flux values for each wavelength
         eflux    (ndarray): An array of error values for each flux value
         rest_frame (float): The rest frame wavelength of the feature
+        unit  (PrefixUnit): Astropy unit to return velocity in (default km / s)
 
     Returns:
         The velocity of the feature
     """
 
+    unit = units.km / units.s if unit is None else unit
     gaussian = lambda x, amplitude, avg, stddev, offset: \
         amplitude * np.exp(-((x - avg) ** 2) / (2 * stddev ** 2)) + offset
 
@@ -97,10 +101,11 @@ def feature_velocity(wave, flux, eflux, rest_frame):
     (amplitude, avg, stddev, offset), cov = curve_fit(
         gaussian, wave, -(flux - 1), p0=start, sigma=eflux)
 
+    c = speed_of_light.to(unit).value
     return 3e5 * (
             ((((rest_frame - avg) / rest_frame) + 1) ** 2 - 1) /
             ((((rest_frame - avg) / rest_frame) + 1) ** 2 + 1)
-    ) / 1000.
+    )
 
 
 def calc_feature_properties(
@@ -128,8 +133,8 @@ def calc_feature_properties(
     rest_frame = line_locations[feat_name]
 
     # Get indices for beginning and end of the feature
-    idx_start, = np.where(wave == feat_start)
-    idx_end, = np.where(wave == feat_end)
+    idx_start = np.where(wave == feat_start)[0][0]
+    idx_end = np.where(wave == feat_end)[0][0]
     if idx_end - idx_start <= 10:
         raise ValueError('Range too small. Please select a wider range')
 
