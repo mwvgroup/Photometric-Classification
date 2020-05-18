b7c77145aeda1bf073a20d2926df6d9d3030aee9
djperrefort <djperrefort@pitt.edu>
2020-05-18 15:47:58 -0400

    Adds typing and subtyping results with host galaxy clipping

---
+++ scripts/run_snid_subtyping.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python3.7
+# -*- coding: UTF-8 -*-
+
+"""This script runs SNID on SDSS Sako et. al 2018 spectra"""
+
+import logging
+import sys
+from pathlib import Path
+
+import pandas as pd
+from astropy.table import Table
+from run_snid_typing import run_snid_on_spectrum, sdss_data_iter
+
+log = logging.getLogger()
+formatter = logging.Formatter(f'%(levelname)8s (%(asctime)s): %(name)s - %(message)s')
+
+file_handler = logging.FileHandler('./snid_subtyping.log')
+file_handler.setFormatter(formatter)
+log.addHandler(file_handler)
+
+log.setLevel(logging.DEBUG)
+
+# Parent types for SNID templates used in the classification
+# E.g. 'Ia' is a parent type, while 'Ia-norm' and 'Ia-91bg' are subtypes
+TYPES = ['Ia', 'Ib', 'Ic', 'II', 'NotSN']
+
+
+def read_peak_type(path):
+    """Return the type summary from an SNID output file
+
+    Args:
+        path (str, Path): Path to read
+
+    Returns:
+         An astropy Table
+    """
+
+    names = ['type', 'ntemp', 'fraction', 'slope', 'redshift',
+             'redshift_error', 'age', 'age_error']
+
+    data = Table.read(
+        str(path), header_start=4, data_start=4,
+        data_end=28, format='ascii.basic', names=names
+    ).to_pandas(index='type')
+
+    # Calculate percentage of templates used for each type
+    # Total matched templates equals the sum of matches for the parent types
+    # (see TYPES global)
+    peak_type = data.loc[TYPES].ntemp.idxmax()
+    total_templates = data.loc[TYPES].ntemp.sum()
+    percent_templates = data.loc[peak_type].ntemp / total_templates
+
+    return peak_type, percent_templates
+
+
+def compile_peak_types(dir, perc_cutoff=0.5):
+    rows = []
+    for path in dir.glob('*snid.output'):
+        obj_id, phase, *_ = path.name.split('_')
+        peak_type, percent_templates = read_peak_type(path)
+        rows.append([obj_id, float(phase), peak_type, percent_templates])
+
+    type_data = pd.DataFrame(
+        rows,
+        columns=['obj_id', 'phase', 'peak_type', 'perc_temp'])
+
+    # Keep only the spectra nearest peak
+    type_data['abs_phase'] = type_data.phase.abs()
+    type_data = type_data.sort_values('abs_phase', ascending=True)
+    type_data = type_data.drop_duplicates(keep='first', subset='obj_id')
+
+    # Apply percentage cutoff
+    type_data = type_data[type_data.perc_temp >= perc_cutoff]
+
+    type_data['obj_id'] = type_data['obj_id'].astype('str')
+    return type_data.set_index('obj_id')
+
+
+def combine_typing_results(*paths, perc_cutoff=0.5):
+    """Combining tables of peak types from SNID
+
+    Values from the later tables take precedence over earlier tables.
+
+    Args:
+        *paths       (Path): Path of the types table
+        perc_cutoff (float): Drop values with ``perctemp`` < this value
+
+    Returns:
+         A pandas data frame with types for each object
+    """
+
+    combined_data = None
+    for path in paths:
+        if combined_data is None:
+            combined_data = compile_peak_types(path, perc_cutoff=perc_cutoff)
+            continue
+
+        new_data = compile_peak_types(path, perc_cutoff=perc_cutoff)
+        combined_data.update(new_data)
+
+    return combined_data
+
+
+def run_snid_subtyping_on_sdss(out_dir, object_types, **kwargs):
+    """Run SNID on SDSS using a single type per object
+
+    ``object_types`` must have a column ``type`` and be indexed by object id.
+
+    Args:
+        out_dir (Path): Directory to write results into
+        object_types (DataFrame): The types to use for each object
+
+    Returns:
+         A pandas data frame with types for each object
+    """
+
+    out_dir.mkdir(exist_ok=True, parents=True)
+    for spec in sdss_data_iter():
+        obj_id = spec.meta['obj_id']
+
+        try:
+            type_record = object_types.loc[obj_id]
+
+        except KeyError:
+            log.info(f'No recorded type for <{obj_id}>')
+            continue
+
+        template_type = type_record['peak_type']
+        log.info(f'Using template {template_type}')
+        run_snid_on_spectrum(out_dir, spec, usetype=template_type, **kwargs)
+
+    # The parameter file is overwritten for each target so is of little use
+    # We remove it to avoid confusion.
+    (out_dir / 'snid.param').unlink()
+
+
+if __name__ == '__main__':
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setFormatter(formatter)
+    log.addHandler(stream_handler)
+
+    results_dir = Path(__file__).resolve().parent.parent / 'results' / 'snid'
+    rlap5_type_paths = results_dir / f'type_rlap_5'
+    rlap10_type_paths = results_dir / f'type_rlap_10'
+
+    log.info('Selecting best fit object types')
+    types = combine_typing_results(rlap5_type_paths, rlap10_type_paths, perc_cutoff=.5)
+
+    for rlap in (5, 10):
+        log.info(f'Subtyping Spectra rlapmin={rlap}')
+        subtype_path = results_dir / f'subtype_rlap_{rlap}'
+        run_snid_subtyping_on_sdss(subtype_path, types, rlapmin=rlap, inter=0, plot=0, verbose=0)
+
+    log.info(f'Subtyping Spectra with exlusively rlapmin=10 results')
+    strict_dir = results_dir / 'subtype_rlap_strict'
+    types = combine_typing_results(rlap10_type_paths, perc_cutoff=.5)
+    run_snid_subtyping_on_sdss(strict_dir, types, rlapmin=10, inter=0, plot=0, verbose=0)
--- scripts/run_snid_typing.py
+++ scripts/run_snid_typing.py
@@ -11,19 +11,14 @@ from datetime import datetime
 from pathlib import Path
 
 import numpy as np
-import pandas as pd
-from astropy.table import Table, vstack
+from astropy.table import Table
 from sndata._utils import convert_to_jd
 from sndata.sdss import sako18spec
 
 log = logging.getLogger()
 formatter = logging.Formatter(f'%(levelname)8s (%(asctime)s): %(name)s - %(message)s')
 
-stream_handler = logging.StreamHandler(sys.stdout)
-stream_handler.setFormatter(formatter)
-log.addHandler(stream_handler)
-
-file_handler = logging.FileHandler('./snid.log')
+file_handler = logging.FileHandler('./snid_typing.log')
 file_handler.setFormatter(formatter)
 log.addHandler(file_handler)
 
@@ -128,17 +123,17 @@ def format_table(table):
 
     # Remove galaxy spectra and spectra outside phase range
     table = table[table['type'] != 'Gal']
+
     table['time'] = convert_sdss_date_to_jd(table['date'])
     table['phase'] = table['time'] - t0
-    table = table[(min_phase <= table['phase']) & (table['phase'] <= max_phase)]
+    table = table[(table['phase'] >= min_phase) & (table['phase'] <= max_phase)]
 
-    # Group data by individual spectra
-    if table:  # Raises error for empty table
-        table.group_by('phase')
-        log.info(f'<{obj_id}> has {len(set(table["phase"]))} spectra')
+    if table:
+        table = table.group_by('phase')
+        log.info(f'Formatted table for <{obj_id}> has {len(table.groups)} spectra')
 
     else:
-	    log.info(f'Formatted table for <{obj_id}> is empty')
+        log.info(f'Formatted table for <{obj_id}> has 0 spectra')
 
     return table
 
@@ -157,14 +152,16 @@ def sdss_data_iter():
     obj_ids = spec_summary[spec_summary['Type'] == 'Ia']['CID']  # Todo: Include all SN types?
     obj_ids = sorted(obj_ids, key=int)
 
-    for obj_id in obj_ids:
-        log.info(f'Fetching data for <{obj_id}>')
+    unique_ids = set(obj_ids)
+    total_ids = len(unique_ids)
+    for i, obj_id in enumerate(unique_ids):
+        log.info(f'Fetching data for <{obj_id}> ({i} / {total_ids})')
 
         data = sako18spec.get_data_for_id(obj_id)
         processed_data = format_table(data)
         if processed_data:
-            for individual_spectrum in processed_data.groups:
-                yield individual_spectrum
+            for spec in processed_data.groups:
+                yield spec
 
         log.debug('Finished with target\n\n')
 
@@ -172,36 +169,6 @@ def sdss_data_iter():
 ###############################################################################
 # Python wrappers for running SNID
 ###############################################################################
-
-# Todo: This was a quick, hacky solution with unnecessarily many type casts
-def read_snid_types(path):
-    """Return the type summary from an SNID output file
-
-    Args:
-        path (str, Path): Path to read
-
-    Returns:
-         An astropy Table
-    """
-
-    names = ['type', 'ntemp', 'fraction', 'slope', 'redshift',
-             'redshift_error', 'age', 'age_error']
-
-    data = Table.read(
-        str(path), header_start=4, data_start=4,
-        data_end=28, format='ascii.basic', names=names
-    )
-
-    dataframe = data.to_pandas(index='type')
-    types = ['Ia', 'Ib', 'Ic', 'II', 'NotSN']
-    num_tempates = dataframe.loc[types].ntemp
-    percent_templates = num_tempates / dataframe.loc[types].ntemp.sum()
-
-    out_data = Table(
-        [types, num_tempates, percent_templates],
-        names=['type', 'ntemp', 'perctemp'])
-
-    return out_data
 
 
 def run_snid_on_spectrum(out_dir, spectrum, **kwargs):
@@ -212,137 +179,40 @@ def run_snid_on_spectrum(out_dir, spectrum, **kwargs):
     Args:
         out_dir   (Path): Directory to write results into
         spectrum (Table): Table with phase, wavelength, and flux columns
-
-    Returns:
-        A Table with SNID type results
     """
 
     obj_id = spectrum.meta['obj_id']
     phase = spectrum['phase'][0]
+    if not len(set(spectrum['phase'])) == 1:
+        raise ValueError('SNID passed multiple spectra')
 
     # Create input file
     snid_input_path = out_dir / f'{obj_id}_{phase:.2f}.dat'
-    snid_out_path = snid_input_path.parent / (snid_input_path.stem + '_snid.output')
 
-    if not snid_out_path.exists():
-        # Run SNID
-        fortran_kwargs = ' '.join(f'{key}={val}' for key, val in kwargs.items())
-        np.savetxt(snid_input_path, spectrum['wavelength', 'flux'])
-        bash_command = f'snid {fortran_kwargs} {snid_input_path}'
-        log.info(bash_command)
-        subprocess.Popen(bash_command.split(), cwd=str(out_dir)).communicate()
+    # Run SNID
+    fortran_kwargs = ' '.join(f'{key}={val}' for key, val in kwargs.items())
+    np.savetxt(snid_input_path, spectrum['wavelength', 'flux'])
+    bash_command = f'snid {fortran_kwargs} {snid_input_path}'
+    log.info(bash_command)
+    subprocess.Popen(bash_command.split(), cwd=str(out_dir)).communicate()
 
-        # Delete input file
-        snid_input_path.unlink()
-
-    else:
-        log.info(f'SNID output already exists at: {snid_out_path}')
-
-    # Parse SNID output file
-    # If statement handles failed SNID run with no output file
-    if snid_out_path.exists():
-        data = read_snid_types(snid_out_path)
-        data['obj_id'] = obj_id
-        data['phase'] = phase
-        data['minwave'] = min(spectrum['wavelength'])
-        data['maxwave'] = max(spectrum['wavelength'])
-        return data
-
-    return Table()
+    # Delete input file
+    snid_input_path.unlink()
 
 
 def run_snid_on_sdss(out_dir, **kwargs):
     """Run SNID for all SDSS spectra classified by Sako18 as ``Ia``
+    
+    forcez and emclip arguments for SNID are set to the SDSS redshift
 
     Args:
         out_dir (Path): Directory to write results into
     """
 
     out_dir.mkdir(exist_ok=True, parents=True)
-    snid_outputs = []
     for spec in sdss_data_iter():
         z = spec.meta['z']
-        snid_outputs.append(run_snid_on_spectrum(out_dir, spec, forcez=z, **kwargs))
-
-    out_path = out_dir / 'all.csv'
-    vstack(snid_outputs).write(out_path, format='ascii.csv', overwrite=True)
-
-    # The parameter file is overwritten for each target so is of little use
-    # We remove it to avoid confusion.
-    (out_dir / 'snid.param').unlink()
-
-
-###############################################################################
-# Logic for Sub-typing spectra
-###############################################################################
-
-
-def read_peak_types(path, perc_cutoff=0.):
-    """Read the top SNID classifications for each objects
-
-    Args:
-        path         (Path): File path to read from
-        perc_cutoff (float): Drop values with ``perctemp`` < this value
-    """
-
-    types = pd.read_csv(path, engine='python')
-
-    # Create a dummy value that allows us to sort measurements by how close
-    # they are to peak phase in asc
-    types['psuedo_phase'] = 100 - types.phase.abs()
-
-    # Sort classifications by number of template matches for each class
-    types.sort_values(['ntemp', 'psuedo_phase'], ascending=False, inplace=True)
-
-    # Keep only the top classifications for each object
-    types.drop_duplicates(keep='first', subset='obj_id', inplace=True)
-
-    # Drop results that don't make the cut
-    types = types[types.perctemp >= perc_cutoff]
-    types['obj_id'] = types['obj_id'].astype('U100')
-
-    return types.set_index('obj_id')
-
-
-def combine_typing_results(*paths, perc_cutoff=0.):
-    """Combining tables of peak types from SNID
-
-    Values from the later tables take precidence over earlier tables.
-
-    Args:
-        *paths       (Path): Path of the types table
-        perc_cutoff (float): Drop values with ``perctemp`` < this value
-
-    Returns:
-         A pandas data frame with types for each object
-    """
-
-    combined_data = read_peak_types(paths[0], perc_cutoff=perc_cutoff)
-    for path in paths[1:]:
-        new_data = read_peak_types(path, perc_cutoff=perc_cutoff)
-        combined_data.update(new_data)
-
-    return combined_data
-
-
-def run_snid_subtyping_on_sdss(out_dir, object_types, **kwargs):
-
-    out_dir.mkdir(exist_ok=True, parents=True)
-    snid_outputs = []
-    for spec in sdss_data_iter():
-        obj_id = spec.meta['obj_id']
-
-        try:
-            type = object_types.loc[obj_id]
-
-        except KeyError:
-            log.info(f'No recorded type for <{obj_id}>')
-            continue
-
-        run_snid_on_spectrum(out_dir, spec, usetype=type['type'], **kwargs)
-
-    out_path = out_dir / 'all.csv'
-    vstack(snid_outputs).write(out_path, format='ascii.csv', overwrite=True)
+        run_snid_on_spectrum(out_dir, spec, forcez=z, emclip=z, **kwargs)
 
     # The parameter file is overwritten for each target so is of little use
     # We remove it to avoid confusion.
@@ -350,21 +220,13 @@ def run_snid_subtyping_on_sdss(out_dir, object_types, **kwargs):
 
 
 if __name__ == '__main__':
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setFormatter(formatter)
+    log.addHandler(stream_handler)
+
     results_dir = Path(__file__).resolve().parent.parent / 'results' / 'snid'
 
-    no_interact_kwargs = dict(inter=0, plot=0, verbose=0)
-
-    type_result_paths = []
-    for rlap in (5, 10):
-        log.info(f'Typing Spectra rlapmin={rlap}')
-        type_path = results_dir / f'type_rlap_{rlap}'
-        run_snid_on_sdss(type_path, rlapmin=rlap, **no_interact_kwargs)
-        type_result_paths.append(type_path / 'all.csv')
-
-    log.info('Selecting best fit object types')
-    types = combine_typing_results(*type_result_paths, perc_cutoff=.5)
-
-    for rlap in (5, 10):
-        log.info(f'Subtyping Spectra rlapmin={rlap}')
-        subtype_path = results_dir / f'subtype_rlap_{rlap}'
-        run_snid_subtyping_on_sdss(subtype_path, types, rlapmin=rlap, **no_interact_kwargs)
+    for rlapmin in (5, 10):
+        log.info(f'Typing Spectra rlapmin={rlapmin}')
+        type_path = results_dir / f'type_rlap_{rlapmin}'
+        run_snid_on_sdss(type_path, rlapmin=rlapmin, inter=0, plot=0, verbose=0)
